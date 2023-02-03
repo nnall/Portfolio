ab47377ff07bac98b9894bb1ed7e7158b3fc779f
nnall <83522890+nnall@users.noreply.github.com>
2023-02-03 09:15:50 -0600

    side-bending nav Grace suggestions

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const nav = document.querySelector('nav');
-const navList = document.querySelector('.nav__list')
+const navList = document.querySelector('.nav__list');
+
+
+
 const navBtn = document.querySelector('.nav__btn');
 
+const navSVG = document.querySelector('.nav__svg')
 
 // Open/Close Nav menu w/.nav_btn
 
@@ -11,6 +15,38 @@ const navBtn = document.querySelector('.nav__btn');
     navList.classList.remove('displayNav'):
     navList.classList.add('displayNav')
  })
+
+
+
+/*
+
+//  Clicking the Nav button
+
+ navBtn.addEventListener('mouseover', ()=>{
+    navBtn.style.transform = "scale(1.1)"      //enlarge with hover
+ })
+
+ navBtn.addEventListener('mousedown', ()=>{
+    navBtn.style.transform = "scale(.9)"        //shrink with down click
+ })
+
+ navBtn.addEventListener('mouseup', ()=>{
+    navBtn.style.transform = "scale(1.1)"       //back to enlarge with unclick
+ })
+
+ navBtn.addEventListener('mouseout', ()=>{
+    navBtn.style.transform = "scale(1)"            //back to normal mouse off
+ })
+
+*/
+
+// still need to know how to have these scale hovering/clicking effects for desktop but possibly not for mobile?
+
+
+// put these navBtn addEventListeners in a parent function, which is only run once viewport is past a certain width?
+
+// Mean you'd need to also run a function to remove the listeners (simple, navBtn.style.transform = "unset") when viewport goes back below that width, they'd otherwise stay attached (right?)
+
 
 
 
