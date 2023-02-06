42544be35a47832afb99af816ff4c8b6eac96d6b
nnall <83522890+nnall@users.noreply.github.com>
2023-02-06 08:19:57 -0600

    testing mobile click state js class

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
+const body = document.querySelector('body');
+
+
 const nav = document.querySelector('nav');
 const navList = document.querySelector('.nav__list');
-
-
-
+const navLinks = document.querySelectorAll('.nav__list__link');
 const navBtn = document.querySelector('.nav__btn');
-
 const navSVG = document.querySelector('.nav__svg')
+const heroBtns = document.querySelectorAll('.hero__btn');
 
-// Open/Close Nav menu w/.nav_btn
 
- navBtn.addEventListener('click', function(){
+
+
+
+// Open/Close Nav menu w/.nav__btn  KEEP THIS LEAVE THIS ALONE - OPENS AND CLOSES NAV MENU
+navBtn.addEventListener('click', function(){
 
     navList.classList.contains('displayNav') ? 
     navList.classList.remove('displayNav'):
@@ -17,28 +21,54 @@ const navSVG = document.querySelector('.nav__svg')
  })
 
 
+// On and off-click effects for mobile nav links, & hero btns
 
-/*
+// USe a resize() funciton? to check page width and apply mobileOnCLick as a callback function?
 
-//  Clicking the Nav button
+// 
 
- navBtn.addEventListener('mouseover', ()=>{
-    navBtn.style.transform = "scale(1.1)"      //enlarge with hover
- })
+// use 1300px 'desktop breakpoint bc just after the widest horizontal tablet width (laptops not until ~2300px)
 
- navBtn.addEventListener('mousedown', ()=>{
-    navBtn.style.transform = "scale(.9)"        //shrink with down click
- })
 
- navBtn.addEventListener('mouseup', ()=>{
-    navBtn.style.transform = "scale(1.1)"       //back to enlarge with unclick
- })
 
- navBtn.addEventListener('mouseout', ()=>{
-    navBtn.style.transform = "scale(1)"            //back to normal mouse off
- })
+// function applying add eventListeners
+const mobileOnClick = function(){
 
-*/
+    // first test whether viewport is below a certain vw. 
+
+
+    if(body.clientWidth < 1300){
+        navLinks.forEach((link)=>{
+            link.addEventListener('ontouchstart', function(){
+                link.classList.add('mobileClickEffect')
+            });
+            link.addEventListener('ontouchend', function(){
+                link.classList.remove('mobileClickEffect');
+            });
+        })
+    
+        heroBtns.forEach((btn)=>{
+            btn.addEventListener('ontouchstart', function(){
+                btn.classList.add('mobileClickEffect')
+            });
+            btn.addEventListener('ontouchend', function(){
+                btn.classList.remove('mobileClickEffect')
+            });
+        })
+    }
+
+}
+
+
+mobileOnClick();
+
+
+
+
+// at that exact breakpoint, there'll be a mediaquery changing applying the hover and :active states in lieu of these .onclick settings. 
+
+
+
 
 // still need to know how to have these scale hovering/clicking effects for desktop but possibly not for mobile?
 
