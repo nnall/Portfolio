19af8549e0b56e63be39c103de4ec396d8f67678
nnall <83522890+nnall@users.noreply.github.com>
2023-02-06 21:17:52 -0600

    Q RE Button not getting mobile class from EL

--- index.js
+++ index.js
@@ -1,69 +1,96 @@
 const body = document.querySelector('body');
 const nav = document.querySelector('nav');
-const navList = document.querySelector('.nav__list');
+const navList = document.querySelector('.nav__list'); /* <ul> */ 
 const navBtn = document.querySelector('.nav__btn');
 const navSVG = document.querySelector('.nav__svg');
 
+// All .nav__list__links
+const navListLinks = document.querySelectorAll('.nav__list__link') 
 
-// ALL <a> on page (including in ".nav__list")
-const pageLinks = document.querySelectorAll('a');
-// const pageLinks = document.querySelectorAll('a:not('nav)');
-
+// ALL <a> on page (NOT including NAV <a> <-- see above)
+const pageLinks = document.querySelectorAll('a:not(.nav__list__link)');
+// console.log(pageLinks);
 
 // All <button> elements except the .nav__btn
-const btns = document.querySelectorAll('button:not(.nav__btn)');  /* dont want .nav__btn getting hover class  */ 
+const btns = document.querySelectorAll('button:not(.nav__btn)');  /* dont want .nav__btn getting .mobileClickEffect settings class when clicked*/ 
 
-
+/*
 console.log(pageLinks);
 console.log(btns);
-
+*/
 
 // OPENS AND CLOSES NAV MENU
 navBtn.addEventListener('click', function(){
-    navList.classList.contains('displayNav') ? 
-    navList.classList.remove('displayNav'):
-    navList.classList.add('displayNav')
+    
+    if(navList.classList.contains('displayNav')){
+        navList.classList.remove('displayNav');
+        navBtn.setAttribute('aria-expanded', 'false')
+    }  
+    else{
+        navList.classList.add('displayNav');
+        navBtn.setAttribute('aria-expanded', 'true')
+    }
+    
+   
  })
 
 //////////////////////////////////////////////////////////
 
 
-
-
-
 // use 1300px 'desktop breakpoint bc just after the widest horizontal tablet width (smallest laptop widths start @ ~2300px)
 
 
-// function applying add eventListeners for nav links click/touch down and up
+// APPLY CLICK EVENT LISTENERS (FOR MOBILE 'CLICK' CLASS)
 const mobileOnClick = function(){
 
     if(body.clientWidth < 1300){    // first ensure the viewport is below a 1300 at loadup. 
-         
+        
+        const addRemoveLoop = function(linkArray){
 
+            /*, {capture:false, passive:true}*/
 
-        // all <a>
-        pageLinks.forEach((link)=>{
-            // touch/click DOWN (add class)
-            link.addEventListener('ontouchstart', function(){
-                link.classList.add('mobileClickEffect')
-            });
-            // touch/click UP (release) (remove class)
-            link.addEventListener('ontouchend', function(){
-                link.classList.remove('mobileClickEffect');
-            });
-        })
+            // .nav__list__links only
+            if(linkArray == navListLinks){ /*<-- gets a different styling class (.navLinkEffect)*/
+                console.log(linkArray)
+                linkArray.forEach((link)=>{
+                    // touch/click DOWN (add class)
+                    link.addEventListener('touchstart', function(){
+                        link.classList.add('navLinkEffect')
+                    })
+                    // touch/click UP (release) (remove class)
+                    link.addEventListener('touchend', function(){
+                        link.classList.remove('navLinkEffect');
+                    });
+                })
+            } 
+            
+            // <button> and <a>
+            else {
+                console.log(linkArray)
+                linkArray.forEach((link)=>{
+                    // touch/click DOWN (add class)
+                    link.addEventListener('touchstart', function(){
+                        link.classList.add('mobileClickEffect')
+                    });
+                    // touch/click UP (release) (remove class)
+                    link.addEventListener('touchend', function(){
+                        link.classList.remove('mobileClickEffect');
+                    });
+                })
+            }
+        }
+        
+        // Adding touchstart/touchend event listeners to..
 
-        // all <button>
-        btns.forEach((btn)=>{
-            // touch/click DOWN (add class)
-            btn.addEventListener('ontouchstart', function(){
-                btn.classList.add('mobileClickEffect')
-            });
-            // touch/click UP (release) (remove class)
-            btn.addEventListener('ontouchend', function(){
-                btn.classList.remove('mobileClickEffect')
-            });
-        })
+        //.nav__list__link <a> elements
+        addRemoveLoop(navListLinks);
+
+        //non-nav <a> elements
+        addRemoveLoop(pageLinks);
+    
+        //<button> elements
+        addRemoveLoop(btns); /*<--- this one's not working.. */
+     
     }
 
 }
@@ -77,14 +104,6 @@ mobileOnClick();
 // in CSS, @1300px MIN-width breakpoint, there'll be a mediaquery applying the hover and :active states applying similar styles to .mobileClickEffect class, but with hover state
 
 
-
-
-// still need to know how to have these scale hovering/clicking effects for desktop but possibly not for mobile?
-
-
-// put these navBtn addEventListeners in a parent function, which is only run once viewport is past a certain width?
-
-// Mean you'd need to also run a function to remove the listeners (simple, navBtn.style.transform = "unset") when viewport goes back below that width, they'd otherwise stay attached (right?)
 
 
 
