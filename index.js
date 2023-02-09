342ff064abd6773bac1ca65cc286ed034a57cdfb
nnall <83522890+nnall@users.noreply.github.com>
2023-02-09 11:19:44 -0600

    styled prevr btns open/close

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const navList = document.querySelector('.nav__list'); /* <ul> */
 const navBtn = document.querySelector('.nav__btn');
 const navSVG = document.querySelector('.nav__svg');
 
+
+const previewer = document.querySelector('.previewer__container');
+const resumeBtn = document.querySelector('.hero__resumeBtn');
+const closeBtn = document.querySelector('.closeBtn');
+
+
 // All .nav__list__links
 const navListLinks = document.querySelectorAll('.nav__list__link') 
 
@@ -14,10 +20,6 @@ const pageLinks = document.querySelectorAll('a:not(.nav__list__link)');
 // All <button> elements except the .nav__btn
 const btns = document.querySelectorAll('button:not(.nav__btn)');  /* dont want .nav__btn getting .mobileClickEffect settings class when clicked*/ 
 
-/*
-console.log(pageLinks);
-console.log(btns);
-*/
 
 // OPENS AND CLOSES NAV MENU
 navBtn.addEventListener('click', function(){
@@ -30,11 +32,19 @@ navBtn.addEventListener('click', function(){
         navList.classList.add('displayNav');
         navBtn.setAttribute('aria-expanded', 'true')
     }
-    
-   
  })
 
- 
+
+//  OPENS/CLOSES PREVIEWER
+resumeBtn.addEventListener('click', ()=>{
+    previewer.classList.add('displayPreviewer');
+})
+
+closeBtn.addEventListener('click', ()=>{
+    previewer.classList.remove('displayPreviewer');
+})
+
+
 
 //////////////////////////////////////////////////////////
 
@@ -48,9 +58,6 @@ const mobileOnClick = function(){
     if(body.clientWidth < 1300){    // first ensure the viewport is below a 1300 at loadup. 
         
         const addRemoveLoop = function(linkArray){
-
-           
-
             // .nav__list__links only
             if(linkArray == navListLinks){ /*<-- gets a different styling class (.navLinkEffect)*/
                 // console.log(linkArray)
@@ -58,7 +65,7 @@ const mobileOnClick = function(){
                     // touch/click DOWN (add class)
                     link.addEventListener('touchstart', function(){
                         link.classList.add('navLinkEffect')
-                    }, {passive:false})
+                    })
                     // touch/click UP (release) (remove class)
                     link.addEventListener('touchend', function(){
                         link.classList.remove('navLinkEffect');
@@ -70,10 +77,11 @@ const mobileOnClick = function(){
             else {
                 // console.log(linkArray)
                 linkArray.forEach((link)=>{
+                    // 
                     // touch/click DOWN (add class)
                     link.addEventListener('touchstart', function(){
                         link.classList.add('mobileClickEffect')                        
-                    }, {passive:false});
+                    });
                     // touch/click UP (release) (remove class)
                     link.addEventListener('touchend', function(){
                         link.classList.remove('mobileClickEffect');
@@ -81,7 +89,6 @@ const mobileOnClick = function(){
                 })
             }
         }
-        
         // Adding touchstart/touchend event listeners to..
         //.nav__list__link <a> elements
         addRemoveLoop(navListLinks);
@@ -89,23 +96,34 @@ const mobileOnClick = function(){
         //non-nav <a> elements
         addRemoveLoop(pageLinks);
     
-        //<button> elements
-        addRemoveLoop(btns); /*<--- this one's not working.. */
-     
+        //<button> elements (includes previewer component buttons)
+        // addRemoveLoop(btns); 
+
     }
-
 }
-
 
 mobileOnClick();
 
 
-
-
-// in CSS, @1300px MIN-width breakpoint, there'll be a mediaquery applying the hover and :active states applying similar styles to .mobileClickEffect class, but with hover state
+// iframe .pdf download & print
 
 
 
+// iframe
+const myIframe = document.getElementById('iframe');
+
+const getPDF = function(){
+    const iframeWindow = myIframe.contentWindow;
+    console.log(iframeWindow)
+
+    console.log(myIframe.contentDocument);
+    // use a DOM method to open the printing dialogue? 
+
+
+
+}
+
+getPDF();
 
 
 
