63ae4b5dc27848b142b5d36f78611d98d529caa5
nnall <83522890+nnall@users.noreply.github.com>
2023-02-13 18:36:51 -0600

    bootstrap question RE undoing BS css

--- index.js
+++ index.js
@@ -17,10 +17,6 @@ const pageLinks = document.querySelectorAll('a:not(.nav__list__link)'); // ALL <
 const btns = document.querySelectorAll('button:not(.nav__btn)');  // All <button> elements except the .nav__btn
 /* dont want .nav__btn getting .mobileClickEffect settings class when clicked*/ 
 
-// PRINT.JS LIBRARY
-
-
-import print from "print-js";
 
 
 
@@ -49,7 +45,6 @@ closeBtn.addEventListener('click', ()=>{
 })
 
 
-// PRINT BUTTON
 
 
 
@@ -57,68 +52,14 @@ closeBtn.addEventListener('click', ()=>{
 //////////////////////////////////////////////////////////
 
 
-// use 1300px 'desktop breakpoint bc just after the widest horizontal tablet width (smallest laptop widths start @ ~2300px)
+// using 1300px 'desktop breakpoint bc just after the widest horizontal tablet width (smallest laptop widths start @ ~2300px)
 
 
-// APPLY CLICK EVENT LISTENERS (FOR MOBILE 'CLICK' CLASS)
-const mobileOnClick = function(){
+// Bootstrap Carousel 
 
-    if(body.clientWidth < 1300){    // first ensure the viewport is below a 1300 at loadup. 
-        
-        const addRemoveLoop = function(linkArray){
-            // .nav__list__links only
-            if(linkArray == navListLinks){ /*<-- gets a different styling class (.navLinkEffect)*/
-                // console.log(linkArray)
-                linkArray.forEach((link)=>{
-                    // touch/click DOWN (add class)
-                    link.addEventListener('touchstart', function(){
-                        link.classList.add('navLinkEffect')
-                    })
-                    // touch/click UP (release) (remove class)
-                    link.addEventListener('touchend', function(){
-                        link.classList.remove('navLinkEffect');
-                        // navList.classList.remove('displayNav');
-                    });
-                })
-            } 
-            
-            // <button> and <a>
-            else {
-                // console.log(linkArray)
-                linkArray.forEach((link)=>{
-                    // touch/click DOWN (add class)
-                    if(!link.classList.contains('closeBtn')){
-                        link.addEventListener('touchstart', function(){
-                            link.classList.add('mobileClickEffect');
-    
-                        });
-                        // touch/click UP (release) (remove class)
-                        link.addEventListener('touchend', function(){
-                            link.classList.remove('mobileClickEffect');
-    
-                        });
-
-                    }    
-                })
-            }
-        }
-        // Adding touchstart/touchend event listeners to..
-        //.nav__list__link <a> elements
-        addRemoveLoop(navListLinks);
-
-        //non-nav <a> elements
-        addRemoveLoop(pageLinks);
-    
-        //<button> elements (includes previewer component buttons)
-        // addRemoveLoop(btns); 
-
-    }
-}
-
-mobileOnClick();
+// $('.carousel').carousel();
 
 
-// iframe .pdf download & print
 
 
 
